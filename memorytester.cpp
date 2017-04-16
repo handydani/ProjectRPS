6da0397d7b29cf588cbee2f64d314b4c3e5a486d
iwilllandonmars <iwilllandonmars@gmail.com>
2017-04-16 17:49:24 -0400

    Create memorytester.cpp

---
+++ memorytester.cpp
@@ -0,0 +1,14 @@
+#include "Memory.h"
+int main(){
+    Memory m = new Memory();
+    int[] file = new file[7];
+    file[0] = 1;
+    file[1] = 2;
+    file[2] = 0;
+    file[4] = 1;
+    file[5] = 2;
+    file[6] = 1;
+    int m = p.getWins(file);
+    cout << m << endl;
+    return 0;
+}
